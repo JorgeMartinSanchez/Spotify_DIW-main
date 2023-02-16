67febf6a8cff0ea31ff5ea46db7afc01a330c436
blayenrique <enrique.blay@juanxxiii.net>
2023-02-16 17:32:17 +0100

    JavaScript Corregido Appends bien hechos y eventos puestos

--- js/scrpits.js
+++ js/scrpits.js
@@ -1,344 +1,62 @@
-// 'use strict'
-// /*
-// +-----------------------------------------------------------+
-// |                                                           |
-// |                   VARIABLES STATEMENTS                    |
-// |                                                           |
-// +-----------------------------------------------------------+
-// */
+window.addEventListener("load", () => {
+  var main = document.getElementsByTagName("main")[0];
+  let intro;
+  let cartas;
+  let carta = "";
+  let cont = 0;
+  fetch("audios.json").then(response => response.json()).then(audios => {
+    audios.forEach(audio => {
+      cont++;
+      if (cont == 1 || cont == 3) {
+        intro = document.createElement("div");
+        intro.classList.add("container-flex");
+        intro.classList.add("text-center");
+        intro.classList.add("col-12");
+      }
+      carta = document.createElement("div");
+      carta.classList.add("music-card");
+      carta.classList.add("col-6");
+      carta.setAttribute("id", "carta");
+        let enlace = document.createElement("a");
+        enlace.setAttribute("id", "enlace");
+        enlace.setAttribute("href", "#");
+          let imagen = document.createElement("img");
+          imagen.setAttribute("src", audio.imagen);
+          let parrafo = document.createElement("p");
+          parrafo.innerHTML=audio.artista + " - " + audio.nombre;
+      
+      enlace.appendChild(imagen);
 
-// const nombres=["Quiero Ser Libre", "Vente Conmigo Gitana", "La Cachimba", "Son Ilusiones"];
-// const canciones=["1 - Quiero.mp3", "2 - Vente.mp3", "3 - Cachimba.mp3", "4 - Ilusiones.mp3"];
-// var cancionChichos=document.querySelector("#cancion");
-// var tituloChichos=document.querySelector("#tituloChichos");
-// var divCanciones=document.querySelector("#canciones");
-// var tema=document.querySelector("#cancion");
-// var barra=document.querySelector("#barra");
-// var barraVolumen=document.querySelector("#volumen");
-// var botonMute=document.querySelector("#mute");
-// var botonVolume=document.querySelector("#volumeMax");
-// var botonAnterior=document.querySelector("#anterior");
-// var botonSiguiente=document.querySelector("#siguiente");
-// var botonAleatorio=document.querySelector("#aleatorio");
-// var botonBucle=document.querySelector("#bucle");
-// var h4s=[];
-// var numerosExcluidos=[];
-// var indice=0;
-// var bucle=false;
-// var aleatorio=false;
+      carta.appendChild(enlace);
+      carta.appendChild(parrafo);
+      intro.appendChild(carta);
+      if (cont == 2 || cont == 4) {
+        main.appendChild(intro);
+      }
+    });
 
-// /*
-// +-----------------------------------------------------------+
-// |                                                           |
-// |                   FUNCTION STATEMENTS                     |
-// |                                                           |
-// +-----------------------------------------------------------+
-// */
+    let primeraFila = main.firstChild;
+    let segundaFila = main.lastChild;
 
-// //Funcion para que carguen las canciones
-// function cargarCancion(i){
-//     cancionChichos.src="audio/"+canciones[i]+"";
-//     tituloChichos.innerHTML=nombres[i];
-//     cancionChichos.currentTime=0;
-//     cancionChichos.play;   
-// }
+    let fary = primeraFila.firstChild;
+    let supermerk = primeraFila.lastChild;
+    let banger = segundaFila.firstChild;
+    let duki = segundaFila.lastChild;
 
-// //Funcion para que la barra se mueva a la par que la musica y que el icono de play se cambie por el de stop si esta sonando y viceversa
-// function reproducir(){
-//     if(tema.paused){
-//         tema.play();
+    fary.addEventListener("click", ()=>{
+      main.innerHTML="";
+    });
 
-//         tema.addEventListener("timeupdate", ()=>{
-//             document.querySelector("#barra").value=tema.currentTime;
-//             barra.max=tema.duration;
-//             //document.querySelector("#actual").innerHTML=tema.currentTime;
-//             //document.querySelector("#final").innerHTML=tema.duration;
-//         });
+    supermerk.addEventListener("click", ()=>{
+      main.innerHTML="";
+    });
 
-//         tema.addEventListener("ended", continuar);
+    banger.addEventListener("click", ()=>{
+      main.innerHTML="";
+    });
 
-//         document.querySelector("#play").src="img/pause.svg";
-//     }else{
-//         document.querySelector("#cancion").pause();
-//         document.querySelector("#play").src="img/play.svg";
-//     }
-// }
-
-// //Funcion para que pase a la siguiente cuando acabe una cancion
-// function continuar(){
-//     tema.removeEventListener("ended", continuar);
-//     if(bucle==false && aleatorio==false){
-//         if(indice==nombres.length-1){
-//             cargarCancion(0);
-//             indice=0;
-//             actualizarSeleccion();
-//         }else{
-//             cargarCancion(indice+1);
-//             indice++;
-//             actualizarSeleccion();
-//         }
-//         tema.play();
-//     }else if(bucle==true && aleatorio==false){
-//         cargarCancion(indice);
-//         reproducir();
-//     }else{
-//         numerosExcluidos.push(indice);
-//         if(numerosExcluidos.length==canciones.length){
-//             numerosExcluidos=[];
-//         }
-//         do{
-//             indice=Math.floor(Math.random() * ((canciones.length-1) - 0 + 1)) + 0;
-//         }while(numerosExcluidos.includes(indice));
-//         console.log(indice+" - He entrado");
-//         console.log(numerosExcluidos);
-//         cargarCancion(indice);
-//         tema.src="audio/"+canciones[indice];
-//         actualizarSeleccion();
-//         reproducir();
-//     }
-// }
-
-// //Funcion para que puedas parar la cancion
-// function parar(){
-//     tema.pause();
-//     tema.currentTime=0;
-//     document.querySelector("#play").src="img/play.svg";
-// }
-
-// //Funcion para que se ponga en rojo la cancion que este sonando
-// function actualizarSeleccion(){
-//     for(let i=0; i<nombres.length; i++){
-//         document.querySelector("#tituloCancion"+i).setAttribute("class", "");
-//     }
-//     document.querySelector("#tituloCancion"+indice).setAttribute("class", "btn_cancion_seleccionada");
-// }
-
-// //Funcion para que vaya alante
-// function avanzar(){
-//     indice++;
-//     if(indice==canciones.length-1){
-//         cargarCancion(0);
-//         tema.src="audio/"+canciones[0];
-//         actualizarSeleccion();
-//         reproducir();
-//     }else{
-//         cargarCancion(indice);
-//         tema.src="audio/"+canciones[indice];
-//         actualizarSeleccion();
-//         reproducir();
-//     }
-// }
-
-// //Funcion para que vuelva atras
-// function retroceder(){
-//     indice--;
-//     if(indice==0){
-//         indice=canciones.length-1;
-//         cargarCancion(indice);
-//         tema.src="audio/"+canciones[indice];
-//         actualizarSeleccion();
-//         reproducir();
-//     }else{
-//         cargarCancion(indice);
-//         tema.src="audio/"+canciones[indice];
-//         actualizarSeleccion();
-//         reproducir();
-//     }
-// }
-
-// //Funcion shuffle
-// function shuffle(){
-//     if(aleatorio==true){
-//         aleatorio=false;
-//         botonAleatorio.setAttribute("class", "");
-//     }else{
-//         aleatorio=true;
-//         bucle=false;
-//         botonBucle.setAttribute("class", "");
-//         botonAleatorio.setAttribute("class", "botonSeleccionado");
-//     }
-// }
-
-// //Funcion Loop
-// function loop(){
-//     if(bucle==true){
-//         bucle=false;
-//         botonBucle.setAttribute("class", "");
-//     }else{
-//         bucle=true;
-//         aleatorio=false;
-//         botonAleatorio.setAttribute("class", "");
-//         botonBucle.setAttribute("class", "botonSeleccionado");
-//     }
-// }
-
-// /*
-// +-----------------------------------------------------------+
-// |                                                           |
-// |                   EVENTS STATEMENTS                       |
-// |                                                           |
-// +-----------------------------------------------------------+
-// */
-
-// document.querySelector("#play").addEventListener("click", reproducir);
-// document.querySelector("#stop").addEventListener("click", parar);
-
-// //Que cada vez que des a la barra la canciÃ³n se mueva
-// barra.addEventListener("change", ()=> {
-//     tema.currentTime=barra.value;
-//     tema.play();
-// });
-
-// //Cambia la opacidad de los botones de volumen en caso de cambio
-// barraVolumen.addEventListener("change", ()=> {
-//     tema.volume=barraVolumen.value;
-//     if(barraVolumen.value>=0.5){
-//         if(barraVolumen.value>0.9){
-//             botonMute.style.opacity=0.1;
-//             botonVolume.style.opacity=0.9
-//         }else{
-//             botonMute.style.opacity=1-barraVolumen.value;
-//             botonVolume.style.opacity=barraVolumen.value;
-//         }
-//     }else{
-//         if(barraVolumen.value<0.1){
-//             botonVolume.style.opacity=0.1;
-//             botonMute.style.opacity=0.9
-//         }else{
-//             botonVolume.style.opacity=barraVolumen.value;
-//             botonMute.style.opacity=1-barraVolumen.value;
-//         }
-//     }
-// });
-
-// //Lo mismo de la opacidad pero pulsando el boton y no la barra
-// botonMute.addEventListener("click", ()=> {
-//     tema.volume=0;
-//     barraVolumen.value=0;
-//     botonMute.style.opacity=0.9;
-//     botonVolume.style.opacity=0.1;
-// });
-
-// //Lo mismo de la opacidad pero pulsando el boton y no la barra
-// botonVolume.addEventListener("click", ()=> {
-//     tema.volume=1;
-//     barraVolumen.value=1;
-//     botonVolume.style.opacity=0.9;
-//     botonMute.style.opacity=0.1;
-// });
-
-// botonAleatorio.addEventListener("click", shuffle);
-// botonAnterior.addEventListener("click", retroceder);
-// botonSiguiente.addEventListener("click", avanzar);
-// botonBucle.addEventListener("click", loop);
-
-
-// /*
-// +-----------------------------------------------------------+
-// |                                                           |
-// |                MAIN EXECUTION STATEMENTS                  |
-// |                                                           |
-// +-----------------------------------------------------------+
-// */
-
-// tema.volume=0.5;
-
-// for(let i=0; i<canciones.length; i++){
-//     let h4=document.createElement("h4");
-//     h4.innerHTML=(i+1)+" - "+nombres[i];
-//     h4.classList.add("btn_cancion");
-//     h4.setAttribute("id", "tituloCancion"+i);
-//     divCanciones.appendChild(h4);
-//     if(i==0){
-//         document.querySelector("#tituloCancion"+i).classList.add("btn_cancion_seleccionada");
-//     }
-//     h4.addEventListener("click", ()=>{
-//         cargarCancion(i);
-//         document.querySelector("#play").src="img/play.svg";
-//         for(x=0; x<canciones.length; x++){
-//             if(h4.innerHTML==h4s[x].innerHTML){
-//                 document.querySelector("#tituloCancion"+x).classList.add("btn_cancion_seleccionada");
-//                 indice=x;
-//                 console.log(indice);
-//             }else{
-//                 document.querySelector("#tituloCancion"+x).classList.remove("btn_cancion_seleccionada");
-//             }
-//         }
-//     });
-//     h4s.push(h4);
-// }
-
-
-var audiosHTML;
-var main = document.getElementsByTagName("main")[0];
-let intro;
-let cartas = document.createElement("span");
-let carta = "";
-let cont = 0;
-fetch("audios.json").then(response => response.json()).then(audios => {
-  audios.forEach(audio => {
-    cont++;
-    if (cont == 1 || cont == 3) {
-      intro = document.createElement("div");
-      intro.classList.add("container-flex");
-      intro.classList.add("text-center");
-      intro.classList.add("col-12");
-    }
-    carta = document.createTextNode(`
-        <div class="music-card col-6">
-          <a id="enlace" href="#"><img src="${audio.imagen}" alt=""></a>
-          <p>${audio.artista} - ${audio.nombre}</p>
-        </div>`);
-    cartas.appendChild(carta);
-    if(cont == 2 || cont == 4){
-      intro.innerHTML=cartas.textContent;
-      cartas.innerHTML="";
-      main.appendChild(intro);
-    }
+    duki.addEventListener("click", ()=>{
+      main.innerHTML="";
+    });
   });
-
-  // Disco Fary 
-  var fary = document.querySelector("body > main > div:nth-child(1) > div:nth-child(1)");
-  fary.addEventListener("click", () => {
-    main.innerHTML = "";
-  });
-  var faryMenu = document.querySelector("body > aside > div.menulink > a:nth-child(2) > span");
-  faryMenu.addEventListener("click", () => {
-    main.innerHTML = "";
-    faryMenu.style.color = "white";
-  });
-
-  // Disco Resacosos
-  var resacosos = document.querySelector("body > main > div:nth-child(1) > div:nth-child(2)");
-  resacosos.addEventListener("click", () => {
-    main.innerHTML = "";
-  });
-  var resacososMenu = document.querySelector("body > aside > div.menulink > a:nth-child(3) > span");
-  resacososMenu.addEventListener("click", () => {
-    main.innerHTML = "";
-    resacososMenu.style.color = "white";
-  });
-
-  // Disco Banger
-  var banger = document.querySelector("body > main > div:nth-child(2) > div");
-  banger.addEventListener("click", () => {
-    main.innerHTML = "";
-  });
-  var bangerMenu = document.querySelector("body > aside > div.menulink > a:nth-child(4) > span");
-  bangerMenu.addEventListener("click", () => {
-    main.innerHTML = "";
-    bangerMenu.style.color = "white";
-  });
-
-  // Disco Fary
-  var duki = document.querySelector("body > main > div:nth-child(2) > div:nth-child(2)");
-  duki.addEventListener("click", () => {
-    main.innerHTML = "";
-  });
-  var dukiMenu = document.querySelector("body > aside > div.menulink > a:nth-child(5) > span");
-  dukiMenu.addEventListener("click", () => {
-    main.innerHTML = "";
-    dukiMenu.style.color = "white";
-  });
-
 });
